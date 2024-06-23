3417e82eeb4a7954fe5b65f127e54f0ee5cd6cc3
João Vitor Seiji Sato Calvaro <joaovssc@gmail.com>
2024-06-23 16:53:12 -0300

    findById implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,11 +1,24 @@
 package model.dao.impl;
 
+import db.DB;
+import db.DbException;
 import java.util.List;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao{
 
+    private Connection conn;
+    
+    public SellerDaoJDBC(Connection conn){
+        this.conn = conn;
+    }
+    
     @Override
     public void insert(Seller obj) {
         throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
@@ -23,8 +36,41 @@ public class SellerDaoJDBC implements SellerDao{
 
     @Override
     public Seller findById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        PreparedStatement st = null;
+        ResultSet rs = null;
+    
+        try{
+            st = conn.prepareStatement("select seller.*, department.Name as DepName " +
+            "from seller inner join " +
+            "department on seller.DepartmentId = department.Id " +
+            "where seller.Id = ?");
+            
+            st.setInt(1, id);
+            rs = st.executeQuery();
+            if(rs.next()){
+                Department dep = new Department();
+                dep.setId(rs.getInt("DepartmentId"));
+                dep.setName(rs.getString("DepName"));
+                
+               Seller obj = new Seller();
+               obj.setId(rs.getInt("Id"));
+               obj.setName(rs.getString("Name"));
+               obj.setBirthDate(rs.getDate("BirthDate"));
+               obj.setBaseSalary(rs.getDouble("BaseSalary"));
+               obj.setDepartment(dep);
+               return obj;
+            }
+            return null;
+        }
+        catch(SQLException e){
+            throw new DbException(e.getMessage());
+        }
+        finally{
+            DB.closeStatement(st);
+            DB.closeResultSet(rs);
+        }
     }
+    
 
     @Override
     public List<Seller> findAll() {
